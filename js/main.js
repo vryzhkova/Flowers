e32463ae25d85c879b3d986656c854a8a98b5330
vryzhkova <73120349+vryzhkova@users.noreply.github.com>
2021-02-24 17:58:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+  const flowerSlider = new Swiper(".flowers-slider", {
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 6,
+
+    // Подключаем стрелки
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // настройки адаптивной версии
+
+    breakpoints: {
+      // wесли браузер >= 320px
+      320: {
+        slidesPerView: 2,
+      },
+      // если браузер >= 480px
+      480: {
+        slidesPerView: 3,
+      },
+
+      992: {
+        slidesPerView: 6,
+      },
+    },
+  });
+
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 1,
+
+    // Подключаем стрелки
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  $("#review-1").on("click", function () {
+    $.fancybox.open(
+      [
+        {
+          src: "https://source.unsplash.com/IvfoDk30JnI/1500x1000",
+          opts: {
+            caption: "First caption",
+            thumb: "https://source.unsplash.com/IvfoDk30JnI/240x160",
+          },
+        },
+        {
+          src: "https://source.unsplash.com/0JYgd2QuMfw/1500x1000",
+          opts: {
+            caption: "Second caption",
+            thumb: "https://source.unsplash.com/0JYgd2QuMfw/240x160",
+          },
+        },
+      ],
+      {
+        loop: true,
+        thumbs: {
+          autoStart: true,
+        },
+      }
+    );
+  });
+});
